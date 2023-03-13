548eb3fe052a7e0a1a2f6540ff1e01aa1edfbab4
Mark Muga <52168180+Raudin@users.noreply.github.com>
2023-03-13 15:00:29 +0300

    Update index.js

---
+++ src/content_option.js
@@ -0,0 +1,142 @@
+
+const logotext = "MUGA";
+const meta = {
+    title: "MARK MUGA",
+    description: "I’m Mark Muga tech enthusiast _ Full stack devloper,currently working in Nairobi",
+};
+
+const introdata = {
+    title: "I’m Mark MUGA",
+    animated: {
+        first: "I'm something of a scientist myself!",
+        second: "... a programmer,",
+        third: "...a tech enthusiast.",
+        fourth: "jrhrhugj",
+    },
+    description: "",
+    your_img_url: "https://media.giphy.com/media/zOvBKUUEERdNm/giphy.gif",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "As a computer science student who is passionate about software development, artificial intelligence, and problem solving, I have honed a set of special skills that prepares me for a variety of jobs in the IT sector. I have a solid foundation in programming and software design thanks to my education in computer science, and my interest in AI has helped me to keep up with the most recent advancements in this quickly-evolving subject. I am a well-rounded candidate for positions in software development, data analysis, and business development because of my technical abilities, capacity for complicated problem-solving, and interest in business. I can't wait to advance professionally, learn more, and have a positive influence on the tech sector.",
+};
+const schooling = [{
+    school: "Jomo Kenyatta University of Africulture and Technology",
+    course: "Computer Science",
+    date: "2019-2023",
+},
+{
+    school: "Cisco Networking Academy",
+    course: "CCNA:Routing & Swithcing",
+    date: "2019-2020",
+},
+{
+    school: "Cisco Networking Academy",
+    course: "Introduction to Cybersecurity",
+    date: "2020-2020",
+},
+];
+
+const worktimeline = [{
+        jobtitle: "INFORMATION TECHNOLOGY INTERN",
+        where: "KCAA",
+        date: "2023",
+    },
+    {
+        jobtitle: "",
+        where: "",
+        date: "",
+    },
+    {
+        jobtitle: "",
+        where: "",
+        date: "",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "web development",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Consultancy",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://legacy.travelnoire.com/wp-content/uploads/2021/11/GettyImages-1170998900.jpg",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+   
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "markmuga72@gmail.com",
+    YOUR_FONE: "+254-748-803-883",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_fqduaws",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/raudin",
+    linkedin: "https://linkedin.com/in/markmuga",
+    twitter: "https://twitter.com/markmuga9",
+    instagram: "https://instagram.com/marrkmuga",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    schooling,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
