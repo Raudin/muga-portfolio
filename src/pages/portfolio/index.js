03dad9ec80d56db1919ef4d84e2673ef82a6de02
raudin <markmuga72@gmail.com>
2023-03-20 11:24:16 +0300

    Updates

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -25,7 +25,8 @@ export const Portfolio = () => {
               <div key={i} className="po_item">
                 <img src={data.img} alt="" />
                 <div className="content">
-                  <p>{data.description}</p>
+                  <h1>{data.description}</h1>
+                  <p>{data.more}</p>
                   <a href={data.link}>view project</a>
                 </div>
               </div>
