48b1453ccbd3e2f74b73e5fbd37035272525ebfe
Mark Muga <52168180+Raudin@users.noreply.github.com>
2024-01-17 23:27:42 +0300

    Update index.js

--- src/components/socialicons/index.js
+++ src/components/socialicons/index.js
@@ -16,42 +16,42 @@ export const Socialicons = (params) => {
       <ul>
         {socialprofils.twitter && (
           <li>
-            <a href={socialprofils.twitter}>
+            <a href={socialprofils.twitter} aria-label="Twitter account">
               <FaTwitter />
             </a>
           </li>
         )}
         {socialprofils.github && (
           <li>
-            <a href={socialprofils.github}>
+            <a href={socialprofils.github} aria-label="Github account">
               <FaGithub />
             </a>
           </li>
         )}
         {socialprofils.facebook && (
           <li>
-            <a href={socialprofils.facebook}>
+            <a href={socialprofils.facebook} aria-label="Facebook account">
               <FaFacebookF />
             </a>
           </li>
         )}
         {socialprofils.linkedin && (
           <li>
-            <a href={socialprofils.linkedin}>
+            <a href={socialprofils.linkedin} aria-label="Linkendin account">
               <FaLinkedin />
             </a>
           </li>
         )}
         {socialprofils.youtube && (
           <li>
-            <a href={socialprofils.youtube}>
+            <a href={socialprofils.youtube} aria-label="Youtube account">
               <FaYoutube />
             </a>
           </li>
         )}
         {socialprofils.twitch && (
           <li>
-            <a href={socialprofils.twitch}>
+            <a href={socialprofils.twitch} aria-label="Twitch account">
               <FaTwitch />
             </a>
           </li>
